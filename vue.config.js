1f8830652f589dff6e39b29dbeedcb1de432a4ca
Joe Zimmerman <contact@joezimjs.com>
2021-09-20 21:00:42 +0000

    Disable hostname check for webpack dev server

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+ module.exports = {
+    configureWebpack: {
+        devServer: {
+            disableHostCheck: true
+        }
+    }
+  }
