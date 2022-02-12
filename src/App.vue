057df7f0e2776faa9eec8daa6d3122c5e8193f9a
Joseph Zimmerman <contact@joezimjs.com>
2022-02-11 21:35:23 -0600

    Make File Input accessible via keyboard

--- src/App.vue
+++ src/App.vue
@@ -117,10 +117,17 @@ label {
 		display: block;
 	}
 
-	input[type=file] {
-		position: absolute;
-		visibility: hidden;
-		pointer-events: none;
+	input[type=file]:not(:focus-visible) {
+		// Visually Hidden Styles taken from Bootstrap 5
+		position: absolute !important;
+		width: 1px !important;
+		height: 1px !important;
+		padding: 0 !important;
+		margin: -1px !important;
+		overflow: hidden !important;
+		clip: rect(0, 0, 0, 0) !important;
+		white-space: nowrap !important;
+		border: 0 !important;
 	}
 
 	.smaller {
